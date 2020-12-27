709102fbbe4710ebf9544843feee0e0d1ed218f1
Vinorval <vinorval@yandex.ru>
2020-12-27 12:02:31 +0300

    feat: сверстать блоки с картачками по новому макету и с адаптивом

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,4 @@
 import { Link } from "react-router-dom";
-import microphone from "../images/микрофон.svg";
-import lightbulb from "../images/лампочка.svg";
-import checkMark from "../images/галочка.svg";
 import leadImage from "../images/lead-image.png";
 import infoPhotoOne from "../images/info-pic-one.png";
 import infoPhotoTwo from "../images/info-pic-two.png";
@@ -55,142 +52,87 @@ function MainPage() {
       </section>
       <section className="irtiative-table">
         <h2 className="irtiative-table__title">
-          Поданные инициативы <span className="irtiative-table__text-number">20</span>
+          Идет голосование
         </h2>
         <ul className="irtiative-table__cards">
           <li className="card">
             <div className="card__texts">
               <p className="card__text">Поставим памятник в конце длинной городской улицы...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+              <div className="card__vote">
+                <p className="card__subtext">уже 1289 голосов</p>
+                <button className="card__button-like"></button>
+              </div>
             </div>
+            <button type="button" className="card__button">
+              Голосовать
+            </button>
           </li>
           <li className="card">
             <div className="card__texts">
               <p className="card__text">Парк известный городской По соседству был с рекой...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+              <div className="card__vote">
+                <p className="card__subtext">уже 74 голосов</p>
+                <button className="card__button-like"></button>
+              </div>
             </div>
+            <button type="button" className="card__button">
+              Голосовать
+            </button>
           </li>
           <li className="card">
             <div className="card__texts">
               <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+              <div className="card__vote">
+                <p className="card__subtext">уже 128 голосов</p>
+                <button className="card__button-like"></button>
+              </div>
             </div>
+            <button type="button" className="card__button">
+              Голосовать
+            </button>
           </li>
           <li className="card">
             <div className="card__texts">
               <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+              <div className="card__vote">
+                <p className="card__subtext">уже 501 голосов</p>
+                <button className="card__button-like"></button>
+              </div>
             </div>
+            <button type="button" className="card__button">
+              Голосовать
+            </button>
           </li>
         </ul>
       </section>
       <section className="irtiative-table">
         <h2 className="irtiative-table__title">
-          Принятые решения <span className="irtiative-table__text-number">10</span>
+          Принятые решения
         </h2>
         <ul className="irtiative-table__cards">
           <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Поставим памятник в конце длинной городской улицы...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+            <div className="card__texts card__texts_type_last">
+              <p className="card__text">Читайте железные книги! Под флейту...</p>
+              <p className="card__subtext card__subtitle_position_last-of-type">сделано 12.11.2020</p>
             </div>
           </li>
           <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Парк известный городской По соседству был с рекой...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+            <div className="card__texts card__texts_type_last">
+              <p className="card__text">Где римский судия судил чужой народ, Стоит базилика...</p>
+              <p className="card__subtext card__subtitle_position_last-of-type">сделано 03.09.2020</p>
             </div>
           </li>
           <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+            <div className="card__texts card__texts_type_last">
+              <p className="card__text">На площади сената — вал сугроба, Дымок костра и холод...</p>
+              <p className="card__subtext card__subtitle_position_last-of-type">сделано 10.12.2020</p>
             </div>
           </li>
           <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
+            <div className="card__texts card__texts_type_last">
+              <p className="card__text">Чудак Евгений бедности стыдится Бензин вдыхает...</p>
+              <p className="card__subtext card__subtitle_position_last-of-type">сделано 02.09.2020</p>
             </div>
-          </li>
-        </ul>
-      </section>
-      <section className="irtiative-table">
-        <h2 className="irtiative-table__title">
-          Идет голосование <span className="irtiative-table__text-number">4</span>
-        </h2>
-        <ul className="irtiative-table__cards">
-          <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Поставим памятник в конце длинной городской улицы...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
-            </div>
-            <button type="button" className="card__button">
-              Голосовать
-            </button>
-          </li>
-          <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Парк известный городской По соседству был с рекой...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
-            </div>
-            <button type="button" className="card__button">
-              Голосовать
-            </button>
-          </li>
-          <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
-            </div>
-            <button type="button" className="card__button">
-              Голосовать
-            </button>
-          </li>
-          <li className="card">
-            <div className="card__texts">
-              <p className="card__text">Как неустойчива судьба Обыкновенной с виду лавочки!...</p>
-              <p className="card__subtext">Всего голосов 300</p>
-              <p className="card__subtext card__subtext_position_last-of-type">
-                Необходимо голосов 1000
-              </p>
-            </div>
-            <button type="button" className="card__button">
-              Голосовать
-            </button>
           </li>
         </ul>
       </section>
